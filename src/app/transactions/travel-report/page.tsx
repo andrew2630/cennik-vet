11e41009e787b9c45fae088c7ba5e51e846342b6
andrew2630 <kontakt@andrzejpudzisz.com>
2025-09-08 17:15:32 +0200

    refactor: remove loading fallback from Suspense in Travel Report page

--- src/app/transactions/travel-report/page.tsx
+++ src/app/transactions/travel-report/page.tsx
@@ -1,16 +1,13 @@
 'use client';
 
 import { Suspense } from 'react';
-import { useTranslations } from 'next-intl';
 import PageWrapper from '@/components/PageWrapper';
 import TravelReportPageContent from '@/components/pages/TravelReportPageContent';
 
 export default function TravelReportPage() {
-  const t = useTranslations('travelReport');
-
   return (
     <PageWrapper>
-      <Suspense fallback={<div>{t('loading')}</div>}>
+      <Suspense>
         <TravelReportPageContent />
       </Suspense>
     </PageWrapper>
